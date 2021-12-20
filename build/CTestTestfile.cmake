2f240df0f2f7dfc4b72ee41014e52a6a600965cc
Kartik <kartikrana9250@gmail.com>
2021-12-20 19:57:44 +0530

    Initial project version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/brad/catkin_ws/src
+# Build directory: /home/brad/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("MR-Robot/mr_robot_description")
+subdirs("MR-Robot/mr_robot_nav")
+subdirs("gazebo_brrr")
+subdirs("MR-Robot/mr_robot_gazebo")
---
+++ build/gazebo_brrr/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/brad/catkin_ws/src/gazebo_brrr
+# Build directory: /home/brad/catkin_ws/build/gazebo_brrr
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
